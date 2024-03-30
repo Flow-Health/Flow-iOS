a4d301713cb776c8e65ff8de5f307ee98b01ed73
Byung jin <bjcho1503@naver.com>
2024-03-30 21:58:42 +0900

    🎨 :: 폰트 리소스 추가

---
+++ Projects/Modules/FlowKit/Resources/Extension/Font+.swift
@@ -0,0 +1,124 @@
+import UIKit
+import SwiftUI
+
+public extension UIFont {
+    // MARK: - Header
+    static let headerH1ExtraBold = FlowKitFontFamily.Pretendard.extraBold.font(size: 30)
+    static let headerH1Bold = FlowKitFontFamily.Pretendard.bold.font(size: 30)
+    static let headerH1SemiBold = FlowKitFontFamily.Pretendard.semiBold.font(size: 30)
+    static let headerH1Medium = FlowKitFontFamily.Pretendard.medium.font(size: 30)
+    static let headerH1Light = FlowKitFontFamily.Pretendard.light.font(size: 30)
+    static let headerH1Thin = FlowKitFontFamily.Pretendard.thin.font(size: 30)
+
+    static let headerH2ExtraBold = FlowKitFontFamily.Pretendard.extraBold.font(size: 24)
+    static let headerH2Bold = FlowKitFontFamily.Pretendard.bold.font(size: 24)
+    static let headerH2SemiBold = FlowKitFontFamily.Pretendard.semiBold.font(size: 24)
+    static let headerH2Medium = FlowKitFontFamily.Pretendard.medium.font(size: 24)
+    static let headerH2Light = FlowKitFontFamily.Pretendard.light.font(size: 24)
+    static let headerH2Thin = FlowKitFontFamily.Pretendard.thin.font(size: 24)
+
+    static let headerH3ExtraBold = FlowKitFontFamily.Pretendard.extraBold.font(size: 20)
+    static let headerH3Bold = FlowKitFontFamily.Pretendard.bold.font(size: 20)
+    static let headerH3SemiBold = FlowKitFontFamily.Pretendard.semiBold.font(size: 20)
+    static let headerH3Medium = FlowKitFontFamily.Pretendard.medium.font(size: 20)
+    static let headerH3Light = FlowKitFontFamily.Pretendard.light.font(size: 20)
+    static let headerH3Thin = FlowKitFontFamily.Pretendard.thin.font(size: 20)
+
+    // MARK: - Body
+    static let bodyB1ExtraBold = FlowKitFontFamily.Pretendard.extraBold.font(size: 16)
+    static let bodyB1Bold = FlowKitFontFamily.Pretendard.bold.font(size: 16)
+    static let bodyB1SemiBold = FlowKitFontFamily.Pretendard.semiBold.font(size: 16)
+    static let bodyB1Medium = FlowKitFontFamily.Pretendard.medium.font(size: 16)
+    static let bodyB1Light = FlowKitFontFamily.Pretendard.light.font(size: 16)
+    static let bodyB1Thin = FlowKitFontFamily.Pretendard.thin.font(size: 16)
+    
+    static let bodyB2ExtraBold = FlowKitFontFamily.Pretendard.extraBold.font(size: 14)
+    static let bodyB2Bold = FlowKitFontFamily.Pretendard.bold.font(size: 14)
+    static let bodyB2SemiBold = FlowKitFontFamily.Pretendard.semiBold.font(size: 14)
+    static let bodyB2Medium = FlowKitFontFamily.Pretendard.medium.font(size: 14)
+    static let bodyB2Light = FlowKitFontFamily.Pretendard.light.font(size: 14)
+    static let bodyB2Thin = FlowKitFontFamily.Pretendard.thin.font(size: 14)
+    
+    static let bodyB3ExtraBold = FlowKitFontFamily.Pretendard.extraBold.font(size: 13)
+    static let bodyB3Bold = FlowKitFontFamily.Pretendard.bold.font(size: 13)
+    static let bodyB3SemiBold = FlowKitFontFamily.Pretendard.semiBold.font(size: 13)
+    static let bodyB3Medium = FlowKitFontFamily.Pretendard.medium.font(size: 13)
+    static let bodyB3Light = FlowKitFontFamily.Pretendard.light.font(size: 13)
+    static let bodyB3Thin = FlowKitFontFamily.Pretendard.thin.font(size: 13)
+
+    // MARK: - Caption
+
+    static let captionC1ExtraBold = FlowKitFontFamily.Pretendard.extraBold.font(size: 12)
+    static let captionC1Bold = FlowKitFontFamily.Pretendard.bold.font(size: 12)
+    static let captionC1SemiBold = FlowKitFontFamily.Pretendard.semiBold.font(size: 12)
+    static let captionC1Medium = FlowKitFontFamily.Pretendard.medium.font(size: 12)
+    static let captionC1Light = FlowKitFontFamily.Pretendard.light.font(size: 12)
+    static let captionC1Thin = FlowKitFontFamily.Pretendard.thin.font(size: 12)
+
+    static let captionC2ExtraBold = FlowKitFontFamily.Pretendard.extraBold.font(size: 10)
+    static let captionC2Bold = FlowKitFontFamily.Pretendard.bold.font(size: 10)
+    static let captionC2SemiBold = FlowKitFontFamily.Pretendard.semiBold.font(size: 10)
+    static let captionC2Medium = FlowKitFontFamily.Pretendard.medium.font(size: 10)
+    static let captionC2Light = FlowKitFontFamily.Pretendard.light.font(size: 10)
+    static let captionC2Thin = FlowKitFontFamily.Pretendard.thin.font(size: 10)
+}
+
+public extension Font {
+    // MARK: - Header
+    static let headerH1ExtraBold = FlowKitFontFamily.Pretendard.extraBold.swiftUIFont(size: 30)
+    static let headerH1Bold = FlowKitFontFamily.Pretendard.bold.swiftUIFont(size: 30)
+    static let headerH1SemiBold = FlowKitFontFamily.Pretendard.semiBold.swiftUIFont(size: 30)
+    static let headerH1Medium = FlowKitFontFamily.Pretendard.medium.swiftUIFont(size: 30)
+    static let headerH1Light = FlowKitFontFamily.Pretendard.light.swiftUIFont(size: 30)
+    static let headerH1Thin = FlowKitFontFamily.Pretendard.thin.swiftUIFont(size: 30)
+    
+    static let headerH2ExtraBold = FlowKitFontFamily.Pretendard.extraBold.swiftUIFont(size: 24)
+    static let headerH2Bold = FlowKitFontFamily.Pretendard.bold.swiftUIFont(size: 24)
+    static let headerH2SemiBold = FlowKitFontFamily.Pretendard.semiBold.swiftUIFont(size: 24)
+    static let headerH2Medium = FlowKitFontFamily.Pretendard.medium.swiftUIFont(size: 24)
+    static let headerH2Light = FlowKitFontFamily.Pretendard.light.swiftUIFont(size: 24)
+    static let headerH2Thin = FlowKitFontFamily.Pretendard.thin.swiftUIFont(size: 24)
+    
+    static let headerH3ExtraBold = FlowKitFontFamily.Pretendard.extraBold.swiftUIFont(size: 20)
+    static let headerH3Bold = FlowKitFontFamily.Pretendard.bold.swiftUIFont(size: 20)
+    static let headerH3SemiBold = FlowKitFontFamily.Pretendard.semiBold.swiftUIFont(size: 20)
+    static let headerH3Medium = FlowKitFontFamily.Pretendard.medium.swiftUIFont(size: 20)
+    static let headerH3Light = FlowKitFontFamily.Pretendard.light.swiftUIFont(size: 20)
+    static let headerH3Thin = FlowKitFontFamily.Pretendard.thin.swiftUIFont(size: 20)
+
+    // MARK: - Body
+    static let bodyB1ExtraBold = FlowKitFontFamily.Pretendard.extraBold.swiftUIFont(size: 16)
+    static let bodyB1Bold = FlowKitFontFamily.Pretendard.bold.swiftUIFont(size: 16)
+    static let bodyB1SemiBold = FlowKitFontFamily.Pretendard.semiBold.swiftUIFont(size: 16)
+    static let bodyB1Medium = FlowKitFontFamily.Pretendard.medium.swiftUIFont(size: 16)
+    static let bodyB1Light = FlowKitFontFamily.Pretendard.light.swiftUIFont(size: 16)
+    static let bodyB1Thin = FlowKitFontFamily.Pretendard.thin.swiftUIFont(size: 16)
+    
+    static let bodyB2ExtraBold = FlowKitFontFamily.Pretendard.extraBold.swiftUIFont(size: 14)
+    static let bodyB2Bold = FlowKitFontFamily.Pretendard.bold.swiftUIFont(size: 14)
+    static let bodyB2SemiBold = FlowKitFontFamily.Pretendard.semiBold.swiftUIFont(size: 14)
+    static let bodyB2Medium = FlowKitFontFamily.Pretendard.medium.swiftUIFont(size: 14)
+    static let bodyB2Light = FlowKitFontFamily.Pretendard.light.swiftUIFont(size: 14)
+    static let bodyB2Thin = FlowKitFontFamily.Pretendard.thin.swiftUIFont(size: 14)
+    
+    static let bodyB3ExtraBold = FlowKitFontFamily.Pretendard.extraBold.swiftUIFont(size: 13)
+    static let bodyB3Bold = FlowKitFontFamily.Pretendard.bold.swiftUIFont(size: 13)
+    static let bodyB3SemiBold = FlowKitFontFamily.Pretendard.semiBold.swiftUIFont(size: 13)
+    static let bodyB3Medium = FlowKitFontFamily.Pretendard.medium.swiftUIFont(size: 13)
+    static let bodyB3Light = FlowKitFontFamily.Pretendard.light.swiftUIFont(size: 13)
+    static let bodyB3Thin = FlowKitFontFamily.Pretendard.thin.swiftUIFont(size: 13)
+
+    // MARK: - Caption
+    static let captionC1ExtraBold = FlowKitFontFamily.Pretendard.extraBold.swiftUIFont(size: 12)
+    static let captionC1Bold = FlowKitFontFamily.Pretendard.bold.swiftUIFont(size: 12)
+    static let captionC1SemiBold = FlowKitFontFamily.Pretendard.semiBold.swiftUIFont(size: 12)
+    static let captionC1Medium = FlowKitFontFamily.Pretendard.medium.swiftUIFont(size: 12)
+    static let captionC1Light = FlowKitFontFamily.Pretendard.light.swiftUIFont(size: 12)
+    static let captionC1Thin = FlowKitFontFamily.Pretendard.thin.swiftUIFont(size: 12)
+    static let captionC2ExtraBold = FlowKitFontFamily.Pretendard.extraBold.swiftUIFont(size: 10)
+    static let captionC2Bold = FlowKitFontFamily.Pretendard.bold.swiftUIFont(size: 10)
+    static let captionC2SemiBold = FlowKitFontFamily.Pretendard.semiBold.swiftUIFont(size: 10)
+    static let captionC2Medium = FlowKitFontFamily.Pretendard.medium.swiftUIFont(size: 10)
+    static let captionC2Light = FlowKitFontFamily.Pretendard.light.swiftUIFont(size: 10)
+    static let captionC2Thin = FlowKitFontFamily.Pretendard.thin.swiftUIFont(size: 10)
+}

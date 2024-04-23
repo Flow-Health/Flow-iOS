6481ec5f3b5dd4ddfe0d61a3b1eb18c542ce3a9d
Byung jin <bjcho1503@naver.com>
2024-04-23 19:11:25 +0900

    📝 :: EmptyView 구현

--- Projects/App/Sources/Scene/BookMarkDetail/BookMarkDetailViewController.swift
+++ Projects/App/Sources/Scene/BookMarkDetail/BookMarkDetailViewController.swift
@@ -14,19 +14,31 @@ class BookMarkDetailViewController: BaseVC<BookMarkDetailViewModel> {
         $0.rowHeight = 120
         $0.keyboardDismissMode = .onDrag
     }
+    private let bookMarkEmptyView = EmptyStatusView(
+        icon: FlowKitAsset.readingGlassesWithCloud.image,
+        title: "등록된 약이 없습니다",
+        subTitle: "약을 검색하여 자주 먹는\n약을 등록해보세요"
+    )
 
     override func attridute() {
         navigationItem.title = "자주 먹는 약"
     }
 
     override func addView() {
-        view.addSubview(resultTableView)
+        view.addSubViews(
+            resultTableView,
+            bookMarkEmptyView
+        )
     }
 
     override func setLayout() {
         resultTableView.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide)
             $0.trailing.leading.bottom.equalToSuperview()
+        }
+        bookMarkEmptyView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(45)
         }
     }
 
@@ -38,6 +50,7 @@ class BookMarkDetailViewController: BaseVC<BookMarkDetailViewModel> {
         let output = viewModel.transform(input: input)
 
         output.bookMarkList.asObservable()
+            .do(onNext: { [weak self] in self?.bookMarkEmptyView.isHidden = !$0.isEmpty })
             .bind(to: resultTableView.rx.items(
                 cellIdentifier: BookMarkResultTableCell.identifier,
                 cellType: BookMarkResultTableCell.self
